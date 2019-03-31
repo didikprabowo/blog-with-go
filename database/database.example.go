5df3148a0f76c54d789913b9272fd9c4f48c5474
Didik Prabowo <didik.prabowo@digitalsiber.id>
2019-03-31 14:14:06 +0700

    example config db

--- database/database.example.go
+++ database/database.example.go
@@ -5,9 +5,9 @@ import _ "github.com/go-sql-driver/mysql"
 
 func MySQL() (db *sql.DB) {
 	dbDriver := "mysql"
-	dbUser := "root"
-	dbPass := "DIDIKprabowo_1995"
-	dbName := "blog"
+	dbUser := ""
+	dbPass := ""
+	dbName := ""
 	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
 	if err != nil {
 		panic(err.Error())
